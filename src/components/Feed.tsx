2359c37552ae4e6d327bad130db57b4c12801f62
kakariko3 <kakariko01@gmail.com>
2022-04-26 17:40:10 +0900

    Update Feed.tsx

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,12 +1,57 @@
+import { useEffect, useState } from 'react';
+import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
+
 import styles from './Feed.module.css';
-import { auth } from '../firebase';
+import { db } from '../firebase';
 import { TweetInput } from './TweetInput';
+import { Post } from './Post';
 
 export const Feed = () => {
+  const [posts, setPosts] = useState([
+    {
+      id: '',
+      avatar: '',
+      image: '',
+      text: '',
+      timestamp: null,
+      username: '',
+    },
+  ]);
+
+  useEffect(() => {
+    // リアルタイムでツイートを取得 (onSnapshot)
+    const postsCollectionRef = collection(db, 'posts');
+    const q = query(postsCollectionRef, orderBy('timestamp', 'desc'));
+    const unsub = onSnapshot(q, (snapshot) => {
+      setPosts(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          avatar: doc.data().avatar,
+          image: doc.data().image,
+          text: doc.data().text,
+          timestamp: doc.data().timestamp,
+          username: doc.data().username,
+        }))
+      );
+    });
+    // クリーンアップ関数 (コンポーネントのアンマウント時に実行)
+    return () => unsub();
+  }, []);
+
   return (
     <div className={styles.feed}>
       <TweetInput />
-      <button onClick={() => auth.signOut()}>Logout</button>
+      {posts.map((post) => (
+        <Post
+          key={post.id}
+          postId={post.id}
+          avatar={post.avatar}
+          image={post.image}
+          text={post.text}
+          timestamp={post.timestamp}
+          username={post.username}
+        />
+      ))}
     </div>
   );
 };
